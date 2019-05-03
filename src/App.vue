22891ba10f5b5087552f8182a86188849c13531b
Alok Ranjan <aloknecessary@gmail.com>
2019-05-03 17:34:38 +0530

    added some dynamic content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Hello there! Welcome to Your Vue.js App"/>
+    <HelloWorld 
+    msg="Hello there! Welcome to Vue.js App"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,32 +2,48 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <span>Created for testing purpose @Alok</span>
+    <br/><br/>
+    <h3>Rendering a list below:</h3>
+    <ul>
+      <li
+        v-for="item in foodList"
+        v-bind:key="item.id"
+      >{{item.id}} - {{item.text}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String,
+    },
+    data() {
+      return {
+        foodList:[{ id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }]
+      }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 20px;
+    line-height: 1.5;
+  }
+  li {
+    display: block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
